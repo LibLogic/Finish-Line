aec801a4a482e0d637ea89481f1c82ff9031456f
Tom Hodgkinson <hdgknsn@gmail.com>
2019-09-25 21:16:49 -0400

    eliminated some redunant event listeners

--- hand.js
+++ hand.js
@@ -40,6 +40,7 @@ function drawCard(startPos = 0, fromEnd = deck.length) {
 
 // Generate a "track" **Do not allow jack, queen, king, ace, or joker in either
 // first 3 or last 3 marker Positions.
+let specialCards = [13, 12, 11, 1, 2];
 function generateHand() {
   let wings = [];
   // Generate wing cards first (excluding J, Q, K, A, or 2)
@@ -123,75 +124,39 @@ document.getElementById("players").addEventListener("keyup", e => {
   }
 });
 
-let markers = document.querySelector(".markers");
-markers.classList.add("hidden");
-
-let selectedMarkerCount = 0;
-let dieChoiceValue = 0;
-document.getElementById("red-marker").addEventListener("click", redMarkerClick);
-function redMarkerClick({ target }) {
-  let markerChoice = "red";
-  selectedMarkerCount++;
-  if (selectedMarkerCount === 2) {
-    dieChoiceValue =
-      dieChoiceValue === redDieValue + blackDieValue + 2
-        ? redDieValue + blackDieValue + 2
-        : redDieValue + blackDieValue + 2 - dieChoiceValue;
-    movePlayer(dieChoiceValue, markerChoice);
-    ++currentPlayer;
-    currentPlayer = currentPlayer % players.length;
-    showRollView();
-    updateMarkers();
-  } else {
-    movePlayer(dieChoiceValue, markerChoice);
-    updateMarkers();
-  }
-  target.classList.add("hidden");
-  sentence.innerHTML = prompts[3];
+function markerEvents() {
+  document
+    .getElementById("green-marker")
+    .addEventListener("click", markerClick);
+  document.getElementById("red-marker").addEventListener("click", markerClick);
+  document.getElementById("blue-marker").addEventListener("click", markerClick);
 }
 
-document
-  .getElementById("green-marker")
-  .addEventListener("click", greenMarkerClick);
-function greenMarkerClick({ target }) {
-  let markerChoice = "green";
-  selectedMarkerCount++;
-  if (selectedMarkerCount === 2) {
-    dieChoiceValue =
-      dieChoiceValue === redDieValue + blackDieValue + 2
-        ? redDieValue + blackDieValue + 2
-        : redDieValue + blackDieValue + 2 - dieChoiceValue;
-    movePlayer(dieChoiceValue, markerChoice);
-    ++currentPlayer;
-    currentPlayer = currentPlayer % players.length;
+let movesRemaining = 2;
+let firstDieValue = 0;
+let secondDieValue = 0;
+function markerClick({ target }) {
+  let markerChoice = target.id;
+  // console.clear();
+  movesRemaining--;
+  if (movesRemaining === 0) {
+    console.log(
+      movesRemaining,
+      secondDieValue,
+      markerChoice,
+      "movesRemaining secondDieValue marker click"
+    );
+    movePlayer(secondDieValue, markerChoice);
+    updateMarkers();
     showRollView();
-    updateMarkers();
   } else {
-    movePlayer(dieChoiceValue, markerChoice);
-    updateMarkers();
-  }
-  target.classList.add("hidden");
-  sentence.innerHTML = prompts[3];
-}
-
-document
-  .getElementById("blue-marker")
-  .addEventListener("click", blueMarkerClick);
-function blueMarkerClick({ target }) {
-  let markerChoice = "blue";
-  selectedMarkerCount++;
-  if (selectedMarkerCount === 2) {
-    dieChoiceValue =
-      dieChoiceValue === redDieValue + blackDieValue + 2
-        ? redDieValue + blackDieValue + 2
-        : redDieValue + blackDieValue + 2 - dieChoiceValue;
-    movePlayer(dieChoiceValue, markerChoice);
-    ++currentPlayer;
-    currentPlayer = currentPlayer % players.length;
-    showRollView();
-    updateMarkers();
-  } else {
-    movePlayer(dieChoiceValue, markerChoice);
+    console.log(
+      movesRemaining,
+      firstDieValue,
+      markerChoice,
+      "movesRemaining firstDieValue marker click"
+    );
+    movePlayer(firstDieValue, markerChoice);
     updateMarkers();
   }
   target.classList.add("hidden");
@@ -199,8 +164,11 @@ function blueMarkerClick({ target }) {
 }
 
 function showRollView() {
+  console.log("running showRollView");
+  displayMarkers();
+
   sentence.classList.add("hidden");
-  markers.classList.add("hidden");
+  document.querySelector(".markers").classList.add("hidden");
   document.querySelector(".black-die").classList.add("dim");
   document.querySelector(".red-die").classList.add("dim");
   document.querySelector(".bullet").classList.add("dim");
@@ -208,7 +176,9 @@ function showRollView() {
 }
 
 function switchUser() {
-  console.log("running switchUser");
+  ++currentPlayer;
+  currentPlayer = currentPlayer % players.length;
+  movesRemaining = 2;
 
   // need to keep this from running when we activate cards
   rollBtn.innerHTML = `${players[currentPlayer].name}<br/>Roll The Dice`;
@@ -229,31 +199,30 @@ function switchUser() {
   });
 }
 
-let sentence = document.getElementById("content");
+let sentence = document.getElementById("prompts");
 
-let diceDiv = document.getElementById("dice");
+let diceContainer = document.getElementById("dice");
 
-let btn1 = document.getElementById("btn1");
+let beginBtn = document.getElementById("begin-btn");
 
-let namesInputDiv = document.getElementById("players-names");
+let playersNames = document.getElementById("players-names");
 
-btn1.addEventListener("click", () => {
+beginBtn.addEventListener("click", () => {
   sentence.innerHTML = prompts[0];
   sentence.classList.remove("hidden");
-  namesInputDiv.classList.remove("hidden");
-  btn1.classList.add("hidden");
+  playersNames.classList.remove("hidden");
+  beginBtn.classList.add("hidden");
 });
 
 function movePlayer(dieMove, markerChoice) {
-  console.log("running movePlayer");
   switch (markerChoice) {
-    case "red":
+    case "red-marker":
       markerToMove = "markerAPos";
       break;
-    case "green":
+    case "green-marker":
       markerToMove = "markerBPos";
       break;
-    case "blue":
+    case "blue-marker":
       markerToMove = "markerCPos";
       break;
   }
@@ -272,12 +241,11 @@ function movePlayer(dieMove, markerChoice) {
   let previousMarkerPosition = players[currentPlayer][markerToMove];
   for (let i = players[currentPlayer][markerToMove] + 1; i < hand.length; i++) {
     if (dieMove === 0) {
-      console.log("dieMove was 0");
       break;
     }
     stopValue = redDieValue + blackDieValue + 2;
     moveCount++;
-    console.clear();
+    // console.clear();
     console.log(
       "Previous Marker position: ",
       previousMarkerPosition + 1,
@@ -298,14 +266,11 @@ function movePlayer(dieMove, markerChoice) {
 
     if (currentCardValue < stopValue) {
       if (moveCount >= dieMove) {
-        console.log("stopped by die value @ ", currentCardValue);
         players[currentPlayer][markerToMove]++;
         break;
       }
-      console.log("increments here, but never stops here");
       players[currentPlayer][markerToMove]++;
     } else {
-      console.log("stopped by card value first @ ", currentCardValue);
       players[currentPlayer][markerToMove]++;
       break;
     }
@@ -313,11 +278,9 @@ function movePlayer(dieMove, markerChoice) {
 
   //check status if good do nothng else come back with previousMarkerPosition
   checkMarkerStatus(dieMove, previousMarkerPosition, currentCard);
-  updateMarkers();
 }
 
 function updateMarkers() {
-  console.log("running updateMarkers");
   let spaces;
   let row = "";
   // for (let i = 0; i < players.length; i++) {
@@ -355,22 +318,23 @@ function updateMarkers() {
       players[currentPlayer].markerCPos
     ].innerHTML += `<span style="color: mediumpurple">•</span>`;
   }
-  if (specialCardFlag === false && selectedMarkerCount > 1) {
+
+  if (specialCardFlag === false && movesRemaining < 1) {
     switchUser();
   }
+  // }
 }
 
 function checkMarkerStatus(dieMove, previousMarkerPosition, currentCard) {
-  console.log("running checkMarkerStatus");
   let handLength = hand.length - 1;
   // first do all the things that should just return
   // remember moves have already been made, just not displayed yet
   if (players[currentPlayer][markerToMove] < handLength) {
-    getSpecialCards(currentCard);
-    if (userCards.length > 0 && selectedMarkerCount > 1) {
-      processSpecialCards();
-    }
-    checkForAttack();
+    // getSpecialCards(currentCard);
+    // if (userCards.length > 0 && movesRemaining < 1) {
+    //   processSpecialCards();
+    // }
+    // checkForAttack();
     return;
   }
   if (previousMarkerPosition + dieMove === handLength) {
@@ -394,9 +358,6 @@ function checkMarkerStatus(dieMove, previousMarkerPosition, currentCard) {
 
 let userCards = [];
 function getSpecialCards(currentCard) {
-  console.log("running getSpecialCards");
-
-  let specialCards = [13, 12, 11, 1, 2];
   currentCardValue = (currentCard % 100) % 14;
   specialCards.forEach(specialCard => {
     if (currentCardValue === specialCard) {
@@ -408,7 +369,6 @@ function getSpecialCards(currentCard) {
 let specialCardFlag = false;
 let furthestMarker = "";
 function processSpecialCards() {
-  console.log("running processSpecialCards");
   console.log(userCards);
 
   let furthestSpecialCard = getFurthestCard();
@@ -513,7 +473,7 @@ function processSpecialCards() {
   }
 
   // be sure to do this last //////////////////////////////
-  if (selectedMarkerCount > 1) {
+  if (movesRemaining < 1) {
     userCards = [];
   }
   // be sure to do this last /////////////////////////////
@@ -558,7 +518,7 @@ function initializePage() {
     '</div><div class="bullet dim">•</div><div class="black-die dim">' +
     dice2 +
     "</div>";
-  diceDiv.innerHTML = dice;
+  diceContainer.innerHTML = dice;
 
   getPlayerNames();
 
@@ -583,9 +543,9 @@ function getPlayerNames() {
 
   class Player {
     constructor(name) {
-      this.markerAPos = -1;
-      this.markerBPos = -1;
-      this.markerCPos = -1;
+      this.markerAPos = 47;
+      this.markerBPos = 47;
+      this.markerCPos = 47;
       this.name = name;
       players.push(this);
     }
@@ -597,8 +557,8 @@ function getPlayerNames() {
     }
   })();
 
-  namesInputDiv.classList.add("hidden");
-  document.getElementById("content").classList.add("hidden");
+  playersNames.classList.add("hidden");
+  document.getElementById("prompts").classList.add("hidden");
 
   renderPlayers();
   renderBoard();
@@ -632,7 +592,7 @@ function specialCardRoll() {
     dice2 +
     "</div>";
 
-  diceDiv.innerHTML = dice;
+  diceContainer.innerHTML = dice;
   document.querySelector(".black-die").classList.remove("dim");
   document.querySelector(".red-die").classList.remove("dim");
 }
@@ -708,11 +668,7 @@ function applyPenalty(furthestMarker) {
 }
 
 function rollDice() {
-  selectedMarkerCount = 0;
-
-  document.getElementById("red-marker").classList.remove("hidden");
-  document.getElementById("blue-marker").classList.remove("hidden");
-  document.getElementById("green-marker").classList.remove("hidden");
+  movesRemaining = 2;
 
   redDieValue = Math.floor(Math.random() * 6);
   let dice1 = dieStr[redDieValue];
@@ -725,69 +681,65 @@ function rollDice() {
     dice2 +
     "</div>";
 
-  diceDiv.innerHTML = dice;
-
+  diceContainer.innerHTML = dice;
   rollBtn.classList.add("hidden");
+  displayMarkers();
+  diceEvents();
+}
 
-  sentence.innerHTML = prompts[2];
+function displayMarkers() {
+  let visibleMarkerCount = 3;
+  players[currentPlayer].markerAPos < hand.length - 1
+    ? document.getElementById("red-marker").classList.remove("hidden")
+    : (document.getElementById("red-marker").classList.add("hidden"),
+      visibleMarkerCount--);
+
+  players[currentPlayer].markerBPos < hand.length - 1
+    ? document.getElementById("green-marker").classList.remove("hidden")
+    : (document.getElementById("green-marker").classList.add("hidden"),
+      visibleMarkerCount--);
+  players[currentPlayer].markerCPos < hand.length - 1
+    ? document.getElementById("blue-marker").classList.remove("hidden")
+    : (document.getElementById("blue-marker").classList.add("hidden"),
+      visibleMarkerCount--);
+
+  console.log(visibleMarkerCount, "visibleMarkerCount running displayMarkers");
+
+  if (visibleMarkerCount < 2) {
+    movesRemaining = 1;
+  }
+  document.querySelector(".markers").classList.remove("hidden");
+}
+
+function diceEvents() {
+  sentence.innerHTML = prompts[2]; // select a die
   sentence.classList.remove("hidden");
 
-  let red = document.querySelector(".red-die");
-  red.addEventListener("click", redDieClick);
+  document.querySelector(".red-die").addEventListener("click", dieClick);
+  document.querySelector(".black-die").addEventListener("click", dieClick);
+  document.querySelector(".bullet").addEventListener("click", dieClick);
+}
 
-  let black = document.querySelector(".black-die");
-  black.addEventListener("click", blackDieClick);
-
-  let green = document.querySelector(".bullet");
-  green.addEventListener("click", bulletClick);
-
-  function bulletClick() {
-    dieChoiceValue = redDieValue + blackDieValue + 2;
-    selectedMarkerCount = 1;
-
-    sentence.innerHTML = prompts[1];
-    sentence.classList.remove("hidden");
-    markers.classList.remove("hidden");
-
-    removeDieClick();
-    red.classList.add("dim");
-    black.classList.add("dim");
+function dieClick({ target }) {
+  if (target.className === "bullet") {
+    secondDieValue = redDieValue + blackDieValue + 2;
+    movesRemaining = 1;
+    target.classList.add("dim");
+    target.classList.add("dim");
   }
-
-  function redDieClick() {
-    dieChoiceValue = redDieValue + 1;
-    selectedMarkerCount = 0;
-    if (selectedMarkerCount == 2) {
-      sentence.innerHTML = prompts[3];
-      sentence.classList.remove("hidden");
-    } else {
-      sentence.innerHTML = prompts[1];
-      sentence.classList.remove("hidden");
-      markers.classList.remove("hidden");
-    }
-    removeDieClick();
-    red.classList.add("dim");
+  if (target.className === "red-die") {
+    firstDieValue = redDieValue + 1;
+    secondDieValue = blackDieValue + 1;
+    target.classList.add("dim");
   }
-
-  function blackDieClick() {
-    dieChoiceValue = blackDieValue + 1;
-    selectedMarkerCount = 0;
-    if (selectedMarkerCount == 2) {
-      sentence.innerHTML = prompts[3];
-      sentence.classList.remove("hidden");
-    } else {
-      sentence.innerHTML = prompts[1];
-      sentence.classList.remove("hidden");
-      markers.classList.remove("hidden");
-    }
-    removeDieClick();
-    black.classList.add("dim");
+  if (target.className === "black-die") {
+    firstDieValue = blackDieValue + 1;
+    secondDieValue = redDieValue + 1;
+    target.classList.add("dim");
   }
-
-  function removeDieClick() {
-    red.removeEventListener("click", redDieClick);
-    black.removeEventListener("click", blackDieClick);
-    green.removeEventListener("click", bulletClick);
-    green.classList.add("dim");
-  }
+  sentence.innerHTML = prompts[1];
+  sentence.classList.remove("hidden");
+  target.removeEventListener("click", dieClick);
+  target.classList.add("dim");
+  markerEvents();
 }
